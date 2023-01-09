f2d01ed8eec1bdba3245caad8d73aca25ae94528
jaehyun0402 <104053445+jaehyun0402@users.noreply.github.com>
2023-01-09 15:30:42 +0900

    Add files via upload
    게임 실행중 UI CS파일

--- Maze_project/Assets/Script/GameDirector.cs
+++ Maze_project/Assets/Script/GameDirector.cs
@@ -1,33 +1,40 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameDirector : MonoBehaviour
 {
-
-    public 
+    public GameObject timerText;
+    public GameObject pointText;
+    public float time = 0f;
+    public int point = 10000;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+       /* this.timerText = GameObject.Find("Time");
+        this.pointText = GameObject.Find("Point");*/
     }
 
     // Update is called once per frame
     void Update()
     {
-     
-        
+        this.time += Time.deltaTime;
+        this.timerText.GetComponent<TextMeshProUGUI>().text = this.time.ToString("F1");
+        this.pointText.GetComponent<TextMeshProUGUI>().text = this.point.ToString();  
     }
 
-    private void OnTriggerEnter(Collider collision)
+    public void DesPoint()
     {
-        Debug.Log("enter");
-        if(collision.gameObject.tag == "end")
-        SceneManager.LoadScene("ClearScene");
-        else
+        if (point > 0)
         {
-            Debug.Log(collision.gameObject.name);
+            point -= 100/2;
+        }
+        else if(point <= 0) 
+        { 
+            point = 0;
         }
     }
 }
